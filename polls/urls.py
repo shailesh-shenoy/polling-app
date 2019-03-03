dd583fabecf26269ec52c24eadd31836a9156c11
shailesh-shenoy <shenoy.shailesh97@gmail.com>
2019-03-03 16:47:19 +0530

    Implemented generic views

--- polls/urls.py
+++ polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     # <domain>/polls/ (prepended for polls app in polling_project)
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     # <domain>/polls/16/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     # <domain>/polls/16/vote/
     path("<int:question_id>/vote", views.vote, name="vote"),
     # <domain>/polls/16/results/
-    path("<int:question_id>/results", views.results, name="results"),
+    path("<int:pk>/results", views.ResultsView.as_view(), name="results"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,32 +3,50 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 
 
-def index(request):
-    """Get latest questions from model, return polls/index template."""
+# def index(request):
+#     """Get latest questions from model, return polls/index template."""
 
-    latest_question_list = Question.objects.order_by("-publication_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    """Get question details for corresponding question_id, return polls/detail template."""
-
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/detail.html", context)
+#     latest_question_list = Question.objects.order_by("-publication_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
 
 
-def results(request, question_id):
-    """Get question for coresponding question id, return poll/results template"""
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/results.html", context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-publication_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    template_name = "polls/detail.html"
+    model = Question
+    # def detail(request, question_id):
+    #     """Get question details for corresponding question_id, return polls/detail template."""
+
+    # question = get_object_or_404(Question, pk=question_id)
+    # context = {"question": question}
+    # return render(request, "polls/detail.html", context)
+
+
+class ResultsView(generic.DetailView):
+    template_name = "polls/results.html"
+    model = Question
+
+
+# def results(request, question_id):
+#     """Get question for coresponding question id, return poll/results template"""
+
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {"question": question}
+#     return render(request, "polls/results.html", context)
 
 
 def vote(request, question_id):
